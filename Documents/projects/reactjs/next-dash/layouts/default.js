73bb3045daee24b755427246438059d83b76adee
gaiyadev <gaiyaobed94@gmail.com>
2021-07-04 12:39:33 +0100

    First commit after setting layouts

---
+++ Documents/projects/reactjs/next-dash/layouts/default.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Default = ({ children }) => {
+  return (
+    <div>
+      <h1>Nave</h1>
+      {children}
+      <h1>Footer</h1>
+    </div>
+  );
+};
+
+export default Default;
